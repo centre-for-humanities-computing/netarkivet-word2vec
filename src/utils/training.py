c883bd0261fea648a81a044bd5d413bf3935e23c
Márton Kardos <power.up1163@gmail.com>
2022-02-21 15:05:48 +0100

    fixing training sequence

--- src/utils/training.py
+++ src/utils/training.py
@@ -57,6 +57,7 @@ def train(
     log: Boolean = True,
     save: Boolean = True,
     sentence_workers=6,
+    verbose=False,
 ):
     """
     Trains word2vec model on ll texts in the supplied files iterable.
@@ -76,26 +77,22 @@ def train(
             texts, window_size=window_size, workers=sentence_workers
         )
         model.build_vocab(corpus_iterable=sentences, update=update)
+        sentences = sentence_stream(
+            texts, window_size=window_size, workers=sentence_workers
+        )
         model.train(
             corpus_iterable=sentences,
             total_examples=model.corpus_count + prev_corpus_count,
             epochs=1,
             compute_loss=True,
-            callbacks=[LogCallback()],
         )
-        # loss = model.get_latest_training_loss()
+        loss = model.get_latest_training_loss()
         if save:
             model.save(f"{save_path}/word2vec.model")
         odd = accuracy_odd_one_out(model)
         sim = accuracy_similarities(model)
         if log:
             wandb.log({"Accuracy - Odd one out": odd, "Similarities R²": sim})
-        print(f"acc_odd: {odd}, sim_r²: {sim}")
+        if verbose:
+            print(f"acc_odd: {odd}, sim_r²: {sim}, loss: {loss}")
         prev_corpus_count = model.corpus_count + prev_corpus_count
-
-
-class LogCallback(CallbackAny2Vec):
-    def on_epoch_end(self, model):
-        loss = model.get_latest_training_loss()
-        wandb.log({"Loss": loss})
-        print(f"Loss: {loss}")
