78e493bdc793f11b87c37847f0ac82c77bc13c99
Márton Kardos <power.up1163@gmail.com>
2022-05-21 11:04:37 +0200

    better logging

--- src/collect_porn_domains.py
+++ src/collect_porn_domains.py
@@ -23,12 +23,17 @@ def main():
     """
     Main function of the script
     """
+    print("----Loading porn classifier----")
     cls = PornClassifier.load(TOPIC_MODEL, TOPIC_MODEL_PATH)
+    print("...")
+    print("----Setting up data stream-----")
     records = stream_all_records(DATA_PATH)
     # Chunking records, so that if something goes south we still got some counts saved
     record_chunks = chunk(records, 150_000)
     total_counts = pd.DataFrame({})
+    print("...")
     for index, record_chunk in enumerate(record_chunks):
+        print("START:")
         # I can totally do this as record_chunk is a list
         # I won't exhaust the stream
         texts = to_text_stream(record_chunks)
@@ -44,7 +49,7 @@ def main():
         # Clear out console
         os.system("clear")
         # Log results of current chunk
-        print(f"Chunk no. {index} yielded the following results:")
+        print(f"\n\nChunk no. {index} yielded the following results:")
         print(current_counts)
         # Adding current counts to total counts
         total_counts = total_counts.add(current_counts, fill_value=0).astype(int)
